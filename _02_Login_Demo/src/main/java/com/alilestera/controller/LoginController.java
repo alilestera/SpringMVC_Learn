c0b1568acc9f9149a07e48f95f58b9b663de2280
Alilestera <alilestera1404@outlook.com>
2022-02-13 23:27:04 +0800

    update loginDemo

--- _02_Login_Demo/src/main/java/com/alilestera/controller/LoginController.java
+++ _02_Login_Demo/src/main/java/com/alilestera/controller/LoginController.java
@@ -1,10 +1,7 @@
 package com.alilestera.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.servlet.http.HttpSession;
 
@@ -17,6 +14,7 @@ public class LoginController {
 
     @PostMapping("/login")
     public String login(String username, String password, HttpSession session) {
+        //也可以用Model来添加attribute
         session.setAttribute("username", username);
         return "/WEB-INF/page/success.jsp";
     }
